64196ff9f25e67926580bf8b43c5360c3cc4daab
Frank FOKOU <frank.fokou@gmail.com>
2022-10-22 23:30:25 +0100

    second commit

--- src/test/java/com/javatechie/IntegrationDevopsApplicationTests.java
+++ src/test/java/com/javatechie/IntegrationDevopsApplicationTests.java
@@ -17,6 +17,7 @@ class IntegrationDevopsApplicationTests {
     @Test
     void contextLoads() {
         logger.info("Test case executing...");
+        logger.info("Test case executing second log statement...");
         assertEquals(true, true);
     }
 
